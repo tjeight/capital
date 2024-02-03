3ade303283a6eff17b3aa5ba18ee3577e5c7c12e
thevgseven <ivgseven@gmail.com>
2024-02-03 17:29:37 +0530

    update

--- src/main/java/org/capital/Main.java
+++ src/main/java/org/capital/Main.java
@@ -3,17 +3,9 @@ package org.capital;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 public class Main extends JFrame {
     private final DefaultTableModel tableModel;
-
-    private static final String DB_CONNECTION_URL = "jdbc:postgresql://ep-green-bush-10717632-pooler.ap-southeast-1.aws.neon.tech/capital?user=vgseven&password=zK3WXCe0gYox&sslmode=require";
-    private static final String DB_USERNAME = "vgseven";
-    private static final String DB_PASSWORD = "zK3WXCe0gYox";
 
     public Main() {
         setTitle("Capital");
@@ -47,41 +39,9 @@ public class Main extends JFrame {
         String date = JOptionPane.showInputDialog("Enter Date:");
         String method = JOptionPane.showInputDialog("Enter Method:");
 
+
         String[] newData = {id, item, amount, date, method };
         tableModel.addRow(newData);
-
-        insertDataIntoDatabase(id, item, amount, date, method);
-    }
-
-    private void insertDataIntoDatabase(String id, String item, String amount, String date, String method) {
-        try {
-
-            Class.forName("org.postgresql.Driver");
-
-            Connection connection = DriverManager.getConnection(DB_CONNECTION_URL, DB_USERNAME, DB_PASSWORD);
-
-            String sql = "INSERT INTO expenses (id, item, amount, date, method) VALUES (?, ?, ?, ?, ?)";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, id);
-                preparedStatement.setString(2, item);
-                preparedStatement.setString(3, amount);
-                preparedStatement.setString(4, date);
-                preparedStatement.setString(5, method);
-
-                int rowsAffected = preparedStatement.executeUpdate();
-
-                if (rowsAffected > 0) {
-                    System.out.println("Record inserted successfully.");
-                    connection.commit();
-                } else {
-                    System.out.println("Failed to insert record.");
-                }
-            }
-
-            connection.close();
-        } catch (ClassNotFoundException | SQLException e) {
-            e.getMessage();
-        }
     }
 
     public static void main(String[] args) {
