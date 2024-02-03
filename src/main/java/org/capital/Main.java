e4f1bc89b5bc8f636f195ee25642ec2d80e57cb1
thevgseven <ivgseven@gmail.com>
2024-02-03 17:31:42 +0530

    update

--- src/main/java/org/capital/Main.java
+++ src/main/java/org/capital/Main.java
@@ -3,6 +3,10 @@ package org.capital;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Main extends JFrame {
     private final DefaultTableModel tableModel;
